429d661d9197c2213e8d40dfe13dee2f4a2c17a9
Nicolas Decoster <nicolas.decoster@magellium.fr>
2018-11-03 00:41:24 +0100

    initial Flight window layout with pokemon images, names and HP

--- src/components/Fight.vue
+++ src/components/Fight.vue
@@ -1,6 +1,20 @@
 <template>
-  <div class="fight">
-    Fight !
+  <div class="root">
+
+    <div class="pokemon-name enemy">
+      BULBIZARRE
+      <br />
+      HP: 7 
+    </div>
+    <img class="pokemon-image sacha" src="@/assets/Salamèche_dos.png">
+
+    <div class="pokemon-name sacha">
+      SALAMECHE
+      <br />
+      HP: 10/10
+    </div>
+    <img class="pokemon-image enemy" src="@/assets/Bulbizarre.png">
+
   </div>
 </template>
 
@@ -10,16 +24,49 @@ export default {
 }
 </script>
 
-<style>
-.fight{
+<style scoped>
+
+.root {
   position: absolute;
   width: 70%;
   height: 70%;
-  top: 11.9%;
-  left: 12.5%;
-  background: #fff;
+  top: 15%;
+  left: 15%;
+  background: #f8f8f8;
   z-index: 2;
-  padding: 20px;
   border:#aaa solid 2px; 
 }
+
+.pokemon-image {
+  image-rendering: pixelated;
+  position: absolute;
+  width: 35%;
+}
+
+.sacha.pokemon-image  {
+  left: 4%;
+  bottom: 30%;
+}
+
+.enemy.pokemon-image {
+  right: 3%;
+  top: 0;;
+}
+
+.pokemon-name {
+  position: absolute;
+  font-size: 180%;
+  color: black;
+}
+
+.sacha.pokemon-name {
+  top: 40%;
+  left: 50%;
+}
+
+.enemy.pokemon-name {
+  top: 0%;
+  left: 5%;
+}
+
 </style>
