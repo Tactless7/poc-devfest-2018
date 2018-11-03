b4d94729739935beab4fb58d5d8fa9eda27ab0fe
Nicolas Decoster <nicolas.decoster@magellium.fr>
2018-11-03 01:44:53 +0100

    start handling fight steps (ask for next move, wait and restart)

--- src/components/Fight.vue
+++ src/components/Fight.vue
@@ -15,17 +15,44 @@
     </div>
     <img class="pokemon-image enemy" src="@/assets/Bulbizarre.png">
 
+    <div class="message">
+      <div class="move-buttons" v-if="step === 'ask for next move'">
+        <button class="next-move-button" v-on:click="moveSelected">CHARGE</button>
+        <br/>
+        <button class="next-move-button" v-on:click="moveSelected">FOUET LIANE</button>
+        <br/>
+        <button class="next-move-button">-</button>
+        <br/>
+        <button class="next-move-button">-</button>
+      </div>
+
+    </div>
+
   </div>
 </template>
 
 <script>
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
 export default {
+  data() {
+    return {
+      step: 'ask for next move'
+    };
+  },
   computed: {
     enemyPokemeonHp() {
       return this.$store.state.enemy.pokemon.hp
     },
     sachaPokemeonHp() {
       return this.$store.state.sacha.pokemon.hp
+    }
+  },
+  methods: {
+    async moveSelected() {
+      this.step = 'show results'
+      await delay(2000)
+      this.step = 'ask for next move';
     }
   }
 }
@@ -76,4 +103,27 @@ export default {
   left: 5%;
 }
 
+.message {
+  position: absolute;
+  width: 85%;
+  height: 15%;
+  bottom: 0;
+  left: 0;
+  border: #888 solid 2px;
+}
+
+.move-buttons {
+  position: absolute;
+  width: 60%;
+  top: 0;
+  right: 0;
+}
+
+.next-move-button {
+  margin-top: -8px;
+  border: 0px;
+  background: transparent;
+  font-family: pokemon;
+  font-size: 160%;
+}
 </style>
