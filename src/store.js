20d0f3db2bb8b157fb871a4cf079c5c1bc452537
Nicolas Decoster <nicolas.decoster@magellium.fr>
2018-11-03 20:22:09 +0100

    restore HP at the end of the battle

--- src/store.js
+++ src/store.js
@@ -8,12 +8,12 @@ export default new Vuex.Store({
     sacha: {
       position: {},
       pokemon: {
-        hp: 9
+        hp: 10
       }
     },
     enemy: {
       pokemon: {
-        hp: 5
+        hp: 10
       } 
     },
     environment: [],
@@ -46,6 +46,12 @@ export default new Vuex.Store({
     DECREASE_ENEMY_POKEMON_HP(state, amount) {
       let newHp = state.enemy.pokemon.hp - amount;
       state.enemy.pokemon.hp = newHp < 0 ? 0 : newHp;
+    },
+    RESTORE_SACHA_POKEMON_HP(state) {
+      state.sacha.pokemon.hp = 10;
+    },
+    RESTORE_ENEMY_POKEMON_HP(state) {
+      state.enemy.pokemon.hp = 10;
     }
   },
 });
